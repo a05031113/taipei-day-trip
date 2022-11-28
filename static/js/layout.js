d235216a201974d8f9a239bcbc7cc024d007c4a8
a05031113 <a05031113@gmail.com>
2022-11-28 19:01:56 +0800

    update version 2.3 for css mistake

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,1 +1,24 @@
-console.log("layout")
+const getRegisterLogin = document.querySelector(".getRegisterLogin");
+const login = document.querySelector(".login");
+const register = document.querySelector(".register");
+const loginLeave = document.querySelector(".loginLeave");
+const registerLeave = document.querySelector(".registerLeave");
+const registerNow = document.querySelector(".registerNow");
+const loginNow = document.querySelector(".loginNow");
+getRegisterLogin.onclick = function(){
+    login.style.display = "flex";
+}
+loginLeave.onclick = function(){
+    login.style.display = "none";
+}
+registerLeave.onclick = function(){
+    register.style.display = "none";
+}
+registerNow.onclick = function(){
+    login.style.display = "none";
+    register.style.display = "flex";
+}
+loginNow.onclick = function(){
+    register.style.display = "none";
+    login.style.display = "flex";
+}
